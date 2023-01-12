69a407dca57742f33da3354798eea70518400ab4
Prajwal V Naik <prajwalv346@gmail.com>
2023-01-12 20:08:04 +0530

    js second file

---
+++ FSJS 2.0 JavaScript Solutions/interploation.js
@@ -0,0 +1,10 @@
+let firstName = "Prajwal";
+let lastName = "Naik";
+let maritalStatus = "Unmarried";
+let country = "India";
+let age = 22;
+
+console.log(`Hello there!! my first name is ${firstName} and ${lastName} is my last name. And I'm from ${country}, a ${maritalStatus} ${age} year old BOY`);
+
+// Then Output be like:
+// Hello there!! my first name is Prajwal and Naik is my last name. And I'm from India, a Unmarried 22 year old BOY
--- FSJS 2.0 JavaScript Solutions/variable.js
+++ FSJS 2.0 JavaScript Solutions/variable.js
@@ -4,6 +4,12 @@ const a = undefined; //Undefined
 const j = null; //null
 
 console.log("p has data type called", typeof(p),"Then value is",p);
-console.log("p has data type called", typeof(r),"Then value is",r);
-console.log("p has data type called", typeof(a),"Then value is",a);
-console.log("p has data type called", typeof(j),"Then value is",j);
+console.log("r has data type called", typeof(r),"Then value is",r);
+console.log("a has data type called", typeof(a),"Then value is",a);
+console.log("j has data type called", typeof(j),"Then value is",j);
+
+// Then Output be like:
+// p has data type called string Then value is Are you coder
+// r has data type called boolean Then value is true
+// a has data type called undefined Then value is undefined   
+// j has data type called object Then value is null
