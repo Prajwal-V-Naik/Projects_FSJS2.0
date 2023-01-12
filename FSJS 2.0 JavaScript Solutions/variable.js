5f570bd4ba6dbbd91ac4824e1836d8a44f30c261
Prajwal V Naik <prajwalv346@gmail.com>
2023-01-12 19:41:28 +0530

    JS folder update

---
+++ FSJS 2.0 JavaScript Solutions/variable.js
@@ -0,0 +1,9 @@
+let p = 'Are you coder'; //String
+let r = true; //Boolean
+const a = undefined; //Undefined
+const j = null; //null
+
+console.log("p has data type called", typeof(p),"Then value is",p);
+console.log("p has data type called", typeof(r),"Then value is",r);
+console.log("p has data type called", typeof(a),"Then value is",a);
+console.log("p has data type called", typeof(j),"Then value is",j);
