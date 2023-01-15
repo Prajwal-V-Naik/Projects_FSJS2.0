fd457913e11a58833bb9dbbdc5a4be578deafef2
Prajwal V Naik <prajwalv346@gmail.com>
2023-01-15 09:29:10 +0530

    JS folder includes()

---
+++ FSJS 2.0 JavaScript Solutions/includes.js
@@ -0,0 +1,11 @@
+let sentence = "The world is full of artificial intelligence, and we may one day be ruled by it";
+let check =  sentence.includes("AI");
+console.log(check);
+
+let checkAgain = sentence.includes("artificial intelligence");
+console.log(checkAgain);
+
+
+// Then Output be like:
+// false
+// true
