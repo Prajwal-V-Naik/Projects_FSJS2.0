d47493701898a66ac9fa13805812e90241b401f0
Prajwal V Naik <prajwalv346@gmail.com>
2023-01-17 09:43:28 +0530

    JS

---
+++ FSJS 2.0 JavaScript Solutions/date.js
@@ -0,0 +1,35 @@
+// - What is the year today?
+let A = new Date();
+let B = A.getFullYear();
+console.log(B)
+// Then Output be like:
+// 2023
+
+
+// - What is the month today as a number?
+let C = A.getMonth();
+console.log(C);
+// Then Output be like:
+// 0
+
+
+//  - What is the date today?
+let D = A.getDate();
+console.log(D);
+// Then Output be like:
+// 17
+
+
+// - What is the day today as a number?
+let E = A.getDay();
+console.log(E);
+// Then Output be like:
+// 2
+
+
+// - What is the hours now?
+let F = A.getHours();
+console.log(F);
+// Then Output be like:
+// 9
+
