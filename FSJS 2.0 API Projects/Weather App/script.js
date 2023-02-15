1ad7829491b2af931d4cea289841f5e2ac16ff7c
Prajwal V Naik <prajwalv346@gmail.com>
2023-02-15 18:31:01 +0530

    History Section Updated

--- FSJS 2.0 API Projects/Weather App/script.js
+++ FSJS 2.0 API Projects/Weather App/script.js
@@ -1,7 +1,38 @@
-// Selectors
+// Selectors and stores
 const time = document.querySelector("#time");
 const aMpM = document.getElementById("aMpM");
+let data;
+let inputs = new Array;
+const inputBox = document.getElementById("inputBox");
+const searchList = document.getElementById("latestSearch");
+const topLocation = document.getElementById("topLocation");
 
+
+
+// Weather
+const getData = async (event) =>{
+    event.preventDefault();
+    if (!inputBox.value) {
+        topLocation.innerText = "";
+        alert("Please Enter The City Name: ");
+        return;
+    }else{
+        topLocation.innerText = inputBox.value;
+        let list = inputBox.value;
+        inputs.push(list);
+        searchList.innerHTML = "";
+        for (var i = Math.max(0, inputs.length - 4); i < inputs.length; i++) {
+            var p = document.createElement("p");
+            p.classList.add("searchHistory");
+            p.textContent = inputs[i];
+            searchList.appendChild(p);
+            p.addEventListener("click", function() {
+                inputBox.value = this.textContent;
+              });
+        }
+    }
+    
+}
 // time
 setInterval( () => {
     let date = new Date();
@@ -23,9 +54,6 @@ setInterval( () => {
 
     let displayTime = hours + ":" + minutes;
     // console.log(displayTime);
-    time.innerHTML = displayTime;
-
-    
-    
-    
+    time.innerHTML = displayTime;    
 },1000);
+
